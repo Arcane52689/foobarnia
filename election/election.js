443ac223e903e75da0451ef47f3f8b53facaa88d
Arcane52689 <tjenkins52689@gmail.com>
2015-12-09 09:32:47 -0500

    add tallying to tally function

--- election/election.js
+++ election/election.js
@@ -1,4 +1,21 @@
 exports.tally = function(votes) {
   // TODO implement tally()
+
+  var polls = {};
+  var vote
+  for (var i = 0; i < votes.length; i++) {
+    vote = votes[i];
+    if (polls[vote]) {
+      polls[vote] = polls[vote] + 1;
+    } else {
+      polls[vote] = 1;
+    }
+  }
+
+
+
+
+
+
   return []
 }
