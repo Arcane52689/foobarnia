e074f97ca6fa9ff8fda8f7daa5b080479f65e434
Arcane52689 <tjenkins52689@gmail.com>
2015-12-09 12:23:28 -0500

    Refactor filter method

--- mileage/auto_seeker.rb
+++ mileage/auto_seeker.rb
@@ -4,12 +4,36 @@ class AutoSeeker
 
   def initialize data
     @data = data
+    autos
   end
 
   def filter options
-    
+    @autos = autos
+    if options['price']
+
+
   end
 
+  def filter_by_low_price low_price
+    @autos.select { |auto| auto.price >= low_price}
+  end
+
+  def filter_by_high_price high_price
+    @autos.select { |auto| auto.price <= high_price}
+  end
+
+  def filter_by_color color
+    @autos.select { |auto| auto.color == color }
+  end
+
+  def filter_by_fuel fuel
+    @autos.select { |auto| auto.fuel == fuel }
+  end
+
+  def filter_by_price price
+    @autos.select { |auto| auto.price == price}
+  end
+  
   def autos
     @autos ||= @data.map do |row|
       Auto.new(row)
